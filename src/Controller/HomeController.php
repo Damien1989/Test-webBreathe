22fe7ffc0ded91df3d180758572177775a0fe1fc
damien <contact.damiendiaz@gmail.com>
2024-04-14 22:13:50 +0200

    finish installation sentry

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\DatadogBundle\DatadogBundle;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
